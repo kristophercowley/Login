08d97206e46e4fb9c2efe8cd08538ac36ae09fcf
Kristopher Cowley <kristopherleecowley@gmail.com>
2016-02-08 11:17:38 -0700

    Login works!

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 var app = angular.module('myapp',[]);
+
+//Declares constant for database reference so if i need to change where it points, I only have to change it here
+app.constant('FBREF', 'https://test-two.firebaseio.com/')
--- controller.js
+++ controller.js
@@ -1,6 +1,31 @@
-app.controller('InputController',function($scope){
+/* global Firebase */
+app.controller('InputController', function ($scope, FBREF) {
+
+    var db = new Firebase(FBREF);
+    console.log("What is FB obj?", db)
+
     $scope.test = "testy";
-    $scope.login = function(){
-        console.log($scope.email,$scope.name)
+
+    function handleDBResponse(err, authData) {
+        console.log('Did we get here?')
+        if (err) {
+            console.log(err)
+            return;
+        }
+        console.log(authData)
     }
+    
+    $scope.register = function(user){
+        db.createUser(user, handleDBResponse)
+    }
+    // We need to take the input from our form and pass it to our database
+    // db is responsible for authentication
+    // after user is authenticated db will send back either error or auth data
+    //We want authData.uid
+    $scope.login = function(user){
+    db.authWithPassword(user, handleDBResponse)
+    }
+
+
+
 })
